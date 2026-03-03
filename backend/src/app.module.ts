0b425366ecf7872eb030d9a42da167b35f20a349
Akash <akashbahadursingh5@gmail.com>
2026-03-03 23:16:31 +0530

    minor changes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { TenantModule } from './tenant/tenant.module';
+import { PlanModule } from './plan/plan.module';
 
 @Module({
-  imports: [PrismaModule,TenantModule],
+  imports: [PrismaModule,TenantModule,PlanModule],
 })
 export class AppModule {}
