8e1c2cb6e00d117bff3fd970dca0900ca98e2dc5
Akash <akashbahadursingh5@gmail.com>
2026-03-03 23:04:54 +0530

    created plan module.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { TenantModule } from './tenant/tenant.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,TenantModule],
 })
 export class AppModule {}
